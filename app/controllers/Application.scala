79c48d327182ace842b2633388ffd37de1a15bc3
Shinobu Aoki <shinobu.aoki@gmail.com>
2012-02-26 03:30:07 +0900

    'init'

---
+++ app/controllers/Application.scala
@@ -0,0 +1,41 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import java.util.{Calendar, Date, Random}
+import models.User
+
+object Application extends Controller {
+  
+  val userForm = Form(
+    mapping(
+      "id" -> longNumber,
+      "name" -> nonEmptyText,
+      "birth" -> date("yyyy/MM/dd")
+    )(User.apply)(User.unapply)
+  )
+  
+  def index = Action {
+    Redirect(routes.Application.list)
+  }
+  
+  def list = Action {
+    val r = new Random(System.nanoTime())
+    val cal = Calendar.getInstance()
+    cal.set(1990, 0, 1, 0, 0, 0)
+    val users = (0 until 20) map { i: Int =>
+      cal.set(Calendar.MONTH, r.nextInt(12))
+      cal.set(Calendar.DATE, r.nextInt(28) + 1)
+      cal.set(Calendar.YEAR, 1970 + r.nextInt(30))
+      User(1001 + i, "Name-" + i, cal.getTime())
+    }
+    Ok(views.html.list(users.toList))
+  }
+  
+  def form = Action {
+    Ok(views.html.form(userForm))
+  }
+  
+}
---
+++ app/models/User.scala
@@ -0,0 +1,5 @@
+package models
+
+import java.util.Date
+
+case class User(id: Long, name: String, birth: Date)
